9b7d159f00a2759e0318ff9dbc0024c2f30ee188
Riley Tomasek <riley.tomasek@gmail.com>
2023-06-06 20:33:26 -0400

    Export schemas and types for bulk method params

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,14 @@ export {
   ChatMessageRoleSchema,
   ChatMessageSchema,
 } from './schemas/chat-completion';
-export { CompletionParamsSchema } from './schemas/completion';
-export { EmbeddingParamsSchema } from './schemas/embedding';
+export {
+  BulkCompletionParamsSchema,
+  CompletionParamsSchema,
+} from './schemas/completion';
+export {
+  BulkEmbeddingParamsSchema,
+  EmbeddingParamsSchema,
+} from './schemas/embedding';
 export { EditParamsSchema } from './schemas/edit';
 
 // Custom OpenAI error
@@ -23,9 +29,14 @@ export type {
   ChatResponseMessage,
 } from './schemas/chat-completion';
 export type {
+  BulkCompletionParams,
   CompletionParams,
   CompletionResponse,
 } from './schemas/completion';
-export type { EmbeddingParams, EmbeddingResponse } from './schemas/embedding';
+export type {
+  BulkEmbeddingParams,
+  EmbeddingParams,
+  EmbeddingResponse,
+} from './schemas/embedding';
 export type { EditParams, EditResponse } from './schemas/edit';
 export type { ConfigOpts } from './openai-client';
