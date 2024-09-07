d21769e7d24b25003978a0ce6f933328f7365fd2
Colin Fortuner <cfortuner@gmail.com>
2024-09-07 13:24:53 -0400

    remove unneeded claude models type

--- src/types.ts
+++ src/types.ts
@@ -68,11 +68,9 @@ export type ChatMessage = {
 export type { AnthropicModelAndUnknown as AnthropicModel }
 export type { ChatModel as OpenAIModel}
 
-type AnthropicModel = | "claude-3-5-sonnet-20240620" | "claude-3-opus-20240229" | "claude-3-sonnet-20240229" | "claude-3-haiku-20240307" | "claude-2.1" | "claude-2.0" | "claude-instant-1.2"
-
 export type ChatParams = Omit<OpenAI.ChatCompletionCreateParams, 'stream' | 'messages' | 'model'> & {
   messages: ChatMessage[];
-  model: ChatModel | AnthropicModel | ({} & string)
+  model: ChatModel | AnthropicModelAndUnknown | ({} & string)
 };
 
 export type ChatResponse = OpenAI.ChatCompletion;
